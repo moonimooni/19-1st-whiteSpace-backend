e33775e8e78c090644b2a8cdf8e8f56f5d178692
ubuntu <a01026676497@gmail.com>
2021-04-15 11:50:37 +0900

    modify : apply models review

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Order(models.Model):
-    user        = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user        = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True))
     product     = models.ManyToManyField('products.Product', through='OrderProduct')
     status      = models.ForeignKey('Status', on_delete=models.CASCADE)
-    address     = models.ForeignKey('users.Address', on_delete=models.CASCADE)
+    address     = models.ForeignKey('users.Address', on_delete=models.CASCADE, null=True, blank=True)
     used_coupon = models.ForeignKey('users.Coupon', on_delete=models.CASCADE, null=True, blank=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at  = models.DateTimeField(auto_now_add=True)
@@ -18,7 +18,7 @@ class OrderProduct(models.Model):
     product    = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     color_size = models.ForeignKey('products.ColorSizeOption', on_delete=models.CASCADE, null=True, blank=True)
     bundle     = models.ForeignKey('products.BundleOption', on_delete=models.CASCADE, null=True, blank=True)
-    quantity   = models.IntegerField()
+    quantity   = models.PositiveIntegerField(default=0)
     
     class Meta:
         db_table = 'order_products'
