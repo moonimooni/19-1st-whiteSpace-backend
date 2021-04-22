4d9eda348a22627e756ef077d6b63790bf0e791b
moonimooni <munhw1408@gmail.com>
2021-04-22 16:52:11 +0900

    Modify : apply transaction decorator

--- orders/order_views.py
+++ orders/order_views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.db            import transaction
 from django.http.response import JsonResponse
 from django.views         import View
 
@@ -8,39 +9,20 @@ from users.utils     import login_decorator
 
 class OrderFromCartView(View):
     @login_decorator
+    @transaction.atomic
     def post(self, request):
         try:
             user           = request.user
             data           = json.loads(request.body)
 
-            cart_id        = data['cart_id']
-            total_price    = data['total_price']
             postal_code    = data['postal_code']
             main_address   = data['main_address']
             detail_address = data['detail_address']
             phone_number   = data['phone_number']
-            
-            selected_products = request.GET.get('item_id', None)
 
-            cart = Order.objects.get(id=cart_id)
-
-            if selected_products:
-                selected_products = list(map(int, selected_products.split(',')))
-                ordering_items    = cart.orderproduct_set.filter(id__in=selected_products)
-
-                new_order = Order.objects.create(
-                    user = user,
-                    status_id = 5,
-                    total_price = 0
-                )
-                ordering_items.update(order=new_order)
-
-                cart.total_price -= total_price
-                cart.save()
-
-            else:
-                cart.status_id = 5
-                new_order      = cart
+            cart_id        = data['cart_id']
+            cart_items     = data['cart_items']
+            total_price    = data['total_price']
 
             address, created = user.address_set.get_or_create(
                 postal_code    = postal_code, 
@@ -50,9 +32,25 @@ class OrderFromCartView(View):
                 defaults       = {'is_main' : True}
             )
 
-            new_order.address     = address
-            new_order.total_price = total_price
+            new_order = Order.objects.create(
+                user = user,
+                status_id = 5,
+                total_price = 0
+            )
+
+            cart = Order.objects.get(id=cart_id)
+
+            if cart.user != user:
+                return JsonResponse({'MESSAGE' : 'UNAUTHORIZED ACCESS'}, status=401)
+
+            ordering_items    = cart.orderproduct_set.filter(id__in=cart_items)
+            new_order.address = address
             
+            cart.total_price      -= total_price
+            new_order.total_price += total_price
+            
+            ordering_items.update(order_id=new_order.id)
+            cart.save()
             new_order.save()
 
             return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=200)
